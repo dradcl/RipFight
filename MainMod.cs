0bf58fba2a4c9648157ef77963063aa2445e0525
Karma Kitten <53679096+karmakittenx@users.noreply.github.com>
2021-03-03 23:14:31 -0500

    Mouse position teleport

--- MainMod.cs
+++ MainMod.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using MelonLoader;
 using UnityEngine;
 
@@ -6,7 +6,6 @@ namespace RipFight
 {
     public class MainMod : MelonMod
     {
-        private Vector3 spawnLocation = new Vector3(0, 0, 0);
         private int levelNum;
         private Gravity gravity;
         private Crown crown;
@@ -15,9 +14,10 @@ namespace RipFight
         private MultiplayerManager multiplayerManager;
         private SteamStatsAndAchievements steamStats;
         private Weapon weapon;
-        private MultiplayerManager MPM;
+        private Transform[] transform;
 
-
+        private float playerZ;
+        private float playerY;
 
         public override void OnUpdate()
         {
@@ -27,12 +27,14 @@ namespace RipFight
                 
                 //player.Jump();
             }
+
+            // (N) Spawn A Dummy Player
             if (Input.GetKeyDown(KeyCode.N))
             {
-                // Summons a Dummy Player
-                MPM.SpawnPlayerDummy(10);
+                multiplayerManager.SpawnPlayerDummy(10);
                 MelonLogger.Msg("Summoned Pawn");
             }
+
             // (T) Achievement Unlocks
             if (Input.GetKeyDown(KeyCode.T))
             {
@@ -40,16 +42,43 @@ namespace RipFight
                 {
                     steamStats.UnlockAchievement(eAchievement);
                     MelonLogger.Msg($"Unlocked: {eAchievement.ToString()}");
+                }
+            }
 
+            // (Z) Teleport character to mouse position
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                playerZ = worldPosition.z;
+                playerY = worldPosition.y;
+
+                if (playerZ > 0)
+                    playerZ -= 1.8f;
+                else
+                    playerZ += 1.8f;
+
+                if (playerY > 0)
+                    playerY -= 2.0f;
+                else
+                    playerY += 2.0f;
+
+                player = UnityEngine.Object.FindObjectOfType<Controller>();
+
+                foreach (Rigidbody rigidbody in player.movement.rigidbodies)
+                {
+                    rigidbody.position = new Vector3(0, playerY, playerZ);
                 }
             }
         }
 
-
         public override void OnSceneWasInitialized(int level, string sceneName)
         {
-            InitializeGameObjects();
             levelNum = level;
+
+            //player = UnityEngine.Object.FindObjectOfType<Controller>();
+            transform = UnityEngine.Object.FindObjectsOfType<Transform>();
+            //InitializeGameObjects();
 
             MelonLogger.Msg($"Level {sceneName} was loaded");
         }
@@ -62,9 +91,8 @@ namespace RipFight
             gameManager = UnityEngine.Object.FindObjectOfType<GameManager>();
             multiplayerManager = UnityEngine.Object.FindObjectOfType<MultiplayerManager>();
             steamStats = UnityEngine.Object.FindObjectOfType<SteamStatsAndAchievements>();
-            MPM = UnityEngine.Object.FindObjectOfType<MultiplayerManager>();
             // reminder u cant modify weapon ammo shitz
-            // weapon = UnityEngine.Object.FindObjectOfType<Weapon>();
+            //weapon = UnityEngine.Object.FindObjectOfType<Weapon>();
         }
 
     }
