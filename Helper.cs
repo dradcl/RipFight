3c53667d4f8ea211cce79be71a92fa02d98f7e21
Karma Kitten <53679096+karmakittenx@users.noreply.github.com>
2021-03-09 01:33:35 -0500

    Added game weapons and ID search

--- Helper.cs
+++ Helper.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace RipFight
 {
     public class Helper
     {
+        public static string[] playerColors = { "default", "blue", "red" };
         public static string PlayerListToString()
         {
             string playerResult = "";
@@ -15,6 +13,82 @@ namespace RipFight
                 playerResult += player.name + "\n";
 
             return playerResult;
+        }
+
+        public static int MatchIDToWeapon(string weaponName)
+        {
+            string[] weapons = Enum.GetNames(typeof(Weapons));
+            
+            for (int i = 0; i < weapons.Length; i++)
+            {
+                if (weapons[i].ToLower() == weaponName)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        private enum Weapons
+        {
+            Pistol,
+            AK47,
+            Sword,
+            GrenadeLauncher,
+            BlinkDagger,
+            Sniper,
+            Revolver,
+            FreezeShooter,
+            RiotShield,
+            Fan,
+            Shotgun,
+            BallOfDoom,
+            ThrustCannon,
+            BowAndArrow,
+            MachineGun,
+            Lightsaber,
+            Uzi,
+            SilentMinigun,
+            MiniMinigun,
+            Minigun,
+            RicochetRifle,
+            StasisGun,
+            Bazooka,
+            Flamethrower,
+            SnakeRevolver,
+            SnakeGrenadeLauncher,
+            SnakeBazooka,
+            GlueGun,
+            LaserRocketLauncher,
+            GoldSword,
+            GoldMinigun,
+            GoldPistol,
+            CarbineRifle,
+            SnakeMinigun,
+            LavaWhip,
+            LavaBeam,
+            LavaShooter,
+            SnakeShotgun,
+            LavaGrenadeLauncher,
+            SpartanLaser,
+            LavaPistol,
+            BlackHoleCannon,
+            BossTriShot,
+            BossRazorShield,
+            BossTriBeam,
+            BossPulseLauncher = 46,
+            BossShooter,
+            BossBurstShooter,
+            BossWallmaker,
+            BossDubstepGun = 54,
+            BossRainbowSlowshot,
+            BossBounceshot = 58,
+            PumpkinLauncher,
+            BurstRifle,
+            DesertEagle,
+            SawedOffShotgun,
+            Spear
         }
     }
 }
